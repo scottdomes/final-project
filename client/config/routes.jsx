a67a1d6d3e35d3fd82231f91a1b70648190cfd61
scottdomes <scottdomes@gmail.com>
2016-04-18 15:55:47 -0700

    Set up header to test- working!

--- client/config/routes.jsx
+++ client/config/routes.jsx
@@ -3,11 +3,11 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
-var HelloWorld = require('../components/HelloWorld.jsx');
+var LandingHeader = require('../components/LandingHeader.jsx');
 
 var routes = (
   <Router history={hashHistory}>
-    <Route path='/' component={HelloWorld} />
+    <Route path='/' component={LandingHeader} />
   </Router> 
 );
 
--- client/main.jsx
+++ client/main.jsx
@@ -2,4 +2,4 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes.jsx');
 
-ReactDOM.render(routes, document.body);
+ReactDOM.render(routes, document.getElementById('app'));
