d33f6500315946f4c71b3736d45b16bd6d46cec3
Osama Khan <osamakhn@gmail.com>
2016-04-19 21:35:17 -0700

    configure rails asset pipeline to mark the webpack generated build.js as precompiled

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Add precompiled React webpack build
+Rails.application.config.assets.precompile += %w( bundle.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
