d5dac5a9720cbaccf4a3ddc0a74854c6b9c1b8d5
scottdomes <scottdomes@gmail.com>
2016-04-26 08:09:06 -0700

    Clicking vote now destroys any previous votes by that user- however, must make it event-specific

--- app/controllers/api/campsite_votes_controller.rb
+++ app/controllers/api/campsite_votes_controller.rb
@@ -12,24 +12,19 @@ class API::CampsiteVotesController < ApplicationController
   def create
     if CampsiteVote.find_by(user_id: params['user_id']) != nil 
       destroy_vote(params['user_id'])
-    else 
-      properties = {user_id: params['user_id'], camp_site_id: params['id']}
-      campsite_vote = CampsiteVote.new(properties)
-      if campsite_vote.save
-        render json: campsite_vote, status: 201, location: [:api, campsite_vote]
-      else
-        render json: { errors: campsite_vote.errors }, status: 422
-      end
+    end
+    properties = {user_id: params['user_id'], camp_site_id: params['id']}
+    campsite_vote = CampsiteVote.new(properties)
+    if campsite_vote.save
+      render json: campsite_vote, status: 201, location: [:api, campsite_vote]
+    else
+      render json: { errors: campsite_vote.errors }, status: 422
     end
   end
 
   def destroy_vote (user_id)
     campsite_vote = CampsiteVote.find_by(user_id: user_id)
-    if campsite_vote.destroy
-        render json: campsite_vote, status: 201, location: [:api, campsite_vote]
-      else
-        render json: { errors: campsite_vote.errors }, status: 422
-      end
+    campsite_vote.destroy
   end
 
   def update
--- app/controllers/api/date_votes_controller.rb
+++ app/controllers/api/date_votes_controller.rb
@@ -12,24 +12,19 @@ class API::DateVotesController < ApplicationController
   def create
     if DateVote.find_by(user_id: params['user_id']) != nil 
       destroy_vote(params['user_id'])
-    else 
-      properties = {user_id: params['user_id'], event_date_id: params['id']}
-      date_vote = DateVote.new(properties)
-      if date_vote.save
-        render json: date_vote, status: 201, location: [:api, date_vote]
-      else
-        render json: { errors: date_vote.errors }, status: 422
-      end
+    end 
+    properties = {user_id: params['user_id'], event_date_id: params['id']}
+    date_vote = DateVote.new(properties)
+    if date_vote.save
+      render json: date_vote, status: 201, location: [:api, date_vote]
+    else
+      render json: { errors: date_vote.errors }, status: 422
     end
   end
 
   def destroy_vote (user_id)
     date_vote = DateVote.find_by(user_id: user_id)
-    if date_vote.destroy
-        render json: date_vote, status: 201, location: [:api, date_vote]
-      else
-        render json: { errors: date_vote.errors }, status: 422
-      end
+    date_vote.destroy
   end
 
   def update
