b31c1fa5a8d38078a61825bd63fd49b691e329bb
scottdomes <scottdomes@gmail.com>
2016-04-26 07:46:36 -0700

    Posting a vote now deletes it if user already votes, or creates a new vote (for campsites only

--- app/controllers/api/campsite_votes_controller.rb
+++ app/controllers/api/campsite_votes_controller.rb
@@ -10,13 +10,26 @@ class API::CampsiteVotesController < ApplicationController
   end
 
   def create
-    properties = {user_id: params['user_id'], camp_site_id: params['id']}
-    campsite_vote = CampsiteVote.new(properties)
-    if campsite_vote.save
-      render json: campsite_vote, status: 201, location: [:api, campsite_vote]
-    else
-      render json: { errors: campsite_vote.errors }, status: 422
+    if CampsiteVote.find_by(user_id: params['user_id']) != nil 
+      destroy_vote(params['user_id'])
+    else 
+      properties = {user_id: params['user_id'], camp_site_id: params['id']}
+      campsite_vote = CampsiteVote.new(properties)
+      if campsite_vote.save
+        render json: campsite_vote, status: 201, location: [:api, campsite_vote]
+      else
+        render json: { errors: campsite_vote.errors }, status: 422
+      end
     end
+  end
+
+  def destroy_vote (user_id)
+    campsite_vote = CampsiteVote.find_by(user_id: user_id)
+    if campsite_vote.destroy
+        render json: campsite_vote, status: 201, location: [:api, campsite_vote]
+      else
+        render json: { errors: campsite_vote.errors }, status: 422
+      end
   end
 
   def update
