7bbfa930959e3bcbc51f9085c3f2fb0b05ede86a
scottdomes <scottdomes@gmail.com>
2016-04-27 17:23:00 -0700

    Added new demo day seeds and images

--- db/test_seeds.rb
+++ db/test_seeds.rb
@@ -6,48 +6,35 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# User ID 1
 User.create(
-  # fb_id: "10153549453065382",
   fb_id: 123,
+  name: "Scosama Juristochen",
+  picture_path: "https://flavorwire.files.wordpress.com/2014/07/8drake.jpg")
+
+User.create(
+  fb_id: "10153549453065382",
   name: "Scott Domes",
   picture_path: "https://scontent.xx.fbcdn.net/hprofile-xta1/v/t1.0-1/c1.0.50.50/p50x50/10450899_10152181634925382_5440199476018927392_n.jpg?oh=cabfda8e5ba3d3adb1fdb241c5025551&oe=57A18CBE")
 
-# User ID 2
 User.create(
   fb_id: "1581388972173882",
   name: "Alvin Chen",
   picture_path: "https://scontent.xx.fbcdn.net/hprofile-xfa1/v/t1.0-1/c15.0.50.50/p50x50/10354686_10150004552801856_220367501106153455_n.jpg?oh=1b2f428689c126f80a083e30205ef68b&oe=57AD5F2F")
 
-# User ID 3
-User.create(
-  fb_id: "10153899890880129",
-  name: "Osama Khan",
-  picture_path: "https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/12401017_10153681523210129_35074620828172847_n.jpg?oh=b701a22e2b3316e5cf172ce9f7b4d20b&oe=579E84E2")
-
-# User ID 4
-User.create(
-  fb_id: "10156922541070604",
-  name: "Filip Juristovski",
-  picture_path: "https://scontent.xx.fbcdn.net/hprofile-xfa1/v/t1.0-1/p50x50/11742674_10155923764465604_9211855613584821142_n.jpg?oh=fce6aa15d278fb5052c66f2c10c330e0&oe=57A9FE70")
-
-# User ID 5
 User.create(
   fb_id: 1234,
   name: "Iggy Azalea",
   picture_path: "https://germanbeautysabine.files.wordpress.com/2012/05/iggy-azalea-2012-03-26-300x300-e1334071099835.jpg")
 
-# User ID 6
 User.create(
-  fb_id: 1235,
-  name: "Drake",
-  picture_path: "https://flavorwire.files.wordpress.com/2014/07/8drake.jpg")
-
+  fb_id: 2334,
+  name: "Mr. Foos", 
+  picture_path: "https://s-media-cache-ak0.pinimg.com/736x/80/bc/11/80bc118a31cd14a110d308ddedb4f31d.jpg")
 
 # Test-OWN-Event-Voting-False Details
 
 Event.create(
-  name: "May Camping Trip",
+  name: "Test-Own-Event-Voting-False",
   vote_on_location: true,
   vote_on_date: true,
   user_id: 1,
@@ -64,7 +51,7 @@ EventDate.create(
 
 # CampSite ID 1
 CampSite.create(
-  name: "Paradise Valley",
+  name: "Party Central",
   event_id: 1,
   user_id: 1)
 
@@ -84,10 +71,6 @@ Attendance.create(
   user_id: 5,
   event_id: 1)
 
-Attendance.create(
-  user_id: 6,
-  event_id: 1)
-
 Car.create(
   user_id: 4,
   event_id: 1,
@@ -103,25 +86,10 @@ Ride.create(
   event_id: 1,
   car_id: 1)
 
-Car.create(
-  user_id: 2,
-  event_id: 1,
-  passenger_capacity: 5)
-
-Ride.create(
-  user_id: 5,
-  event_id: 1,
-  car_id: 2)
-
-Ride.create(
-  user_id: 6,
-  event_id: 1,
-  car_id: 2)
-
 # Test-OTHER-Event-Voting-False Details
 
 Event.create(
-  name: "Party Time",
+  name: "Test-Other-Event-Voting-False",
   vote_on_location: true,
   vote_on_date: true,
   user_id: 2,
@@ -138,7 +106,7 @@ EventDate.create(
 
 # CampSite ID 2
 CampSite.create(
-  name: "Tofino",
+  name: "Scott's House",
   event_id: 2,
   user_id: 2)
 
@@ -157,17 +125,17 @@ Attendance.create(
 Car.create(
   user_id: 3,
   event_id: 2,
-  passenger_capacity: 4)
+  passenger_capacity: 13)
 
 Ride.create(
   user_id: 1,
   event_id: 2,
-  car_id: 3)
+  car_id: 2)
 
 Ride.create(
   user_id: 4,
   event_id: 2,
-  car_id: 3)
+  car_id: 2)
 
 Car.create(
   user_id: 2,
@@ -177,7 +145,7 @@ Car.create(
 Ride.create(
   user_id: 5,
   event_id: 2,
-  car_id: 4)
+  car_id: 3)
 
 
 
@@ -185,7 +153,7 @@ Ride.create(
 # Test-OWN-Event-Voting-True Details
 
 Event.create(
-  name: "Lighthouse Grad Roadtrip",
+  name: "Test-Own-Event-Voting-True",
   vote_on_location: true,
   vote_on_date: true,
   user_id: 1,
@@ -200,42 +168,17 @@ EventDate.create(
   event_id: 3,
   user_id: 1)
 
-DateVote.create(
-  event_date_id: 3,
-  user_id: 2,
-  event_id: 3)
-
-DateVote.create(
-  event_date_id: 3,
-  user_id: 5,
-  event_id: 3)
-
 # CampSite ID 3
 CampSite.create(
-  name: "Kelowna",
+  name: "A Bar",
   event_id: 3,
   user_id: 1)
 
-CampsiteVote.create(
-  camp_site_id: 3,
-  user_id: 1,
-  event_id: 3)
-
-CampsiteVote.create(
-  camp_site_id: 3,
-  user_id: 2,
-  event_id: 3)
-
 # CampSite ID 4
 CampSite.create(
-  name: "Osoyoos",
+  name: "A Different Bar",
   event_id: 3,
   user_id: 2)
-
-CampsiteVote.create(
-  camp_site_id: 4,
-  user_id: 3,
-  event_id: 3)
 
 Attendance.create(
   user_id: 2,
@@ -257,7 +200,7 @@ Attendance.create(
 #Test-OTHER-Event-Voting-True
 
 Event.create(
-  name: "Let's Climb a Mountain",
+  name: "Test-Other-Event-Voting-True",
   vote_on_location: true,
   vote_on_date: true,
   user_id: 3,
@@ -274,32 +217,32 @@ EventDate.create(
 
 # EventDate ID 5
 EventDate.create(
-  start_date: "May 20th 2016",
-  end_date: "May 22th 2016",
+  start_date: "May 11th 2016",
+  end_date: "May 12th 2016",
   event_id: 4,
   user_id: 2)
 
 # CampSite ID 5
 CampSite.create(
-  name: "Seymour Mountain",
+  name: "Alvin's House",
   event_id: 4,
   user_id: 3)
 
 # CampSite ID 6
 CampSite.create(
-  name: "Grouse Mountain",
+  name: "Scott's House",
   event_id: 4,
   user_id: 2)
 
 # CampSite ID 7
 CampSite.create(
-  name: "Mount Fromme",
+  name: "Scosama's House",
   event_id: 4,
   user_id: 1)
 
 # CampSite ID 8
 CampSite.create(
-  name: "Cypress Mountain",
+  name: "Iggy's House",
   event_id: 4,
   user_id: 4)
 
@@ -336,3 +279,51 @@ Attendance.create(
 Attendance.create(
   user_id: 4,
   event_id: 4)
+
+# Location-Voting-No-Date
+
+Event.create(
+  name: "Location-Voting-No-Date",
+  vote_on_location: true,
+  vote_on_date: false,
+  user_id: 1,
+  final_location_id: 9,
+  final_date_id: 6,
+  voting_phase: true)
+
+# CampSite ID 9
+CampSite.create(
+  name: "Iggy's House",
+  event_id: 5,
+  user_id: 1)
+
+# EventDate ID 6
+EventDate.create(
+  start_date: "May 11th 2016",
+  end_date: "May 12th 2016",
+  event_id: 5,
+  user_id: 1)
+
+# Date-Voting-No-Location
+
+Event.create(
+  name: "Date-Voting-No-Location",
+  vote_on_location: false,
+  vote_on_date: true,
+  user_id: 1,
+  final_location_id: 10,
+  final_date_id: 7,
+  voting_phase: true)
+
+# CampSite ID 10
+CampSite.create(
+  name: "Iggy's House",
+  event_id: 6,
+  user_id: 1)
+
+# EventDate ID 7
+EventDate.create(
+  start_date: "May 11th 2016",
+  end_date: "May 12th 2016",
+  event_id: 6,
+  user_id: 1)
